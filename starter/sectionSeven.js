cc7fbd719e9067a7c85e3f78be441c3df7e46c71
Levi Adoptante <36682474+theleviathan75@users.noreply.github.com>
2021-02-10 22:10:39 -0500

    Update sectionSeven.js

--- starter/sectionSeven.js
+++ starter/sectionSeven.js
@@ -11,37 +11,35 @@ const displayMessage = function (message) {
 };
 
 document.querySelector('.check').addEventListener('click', function () {
-  if (playing) {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-    // When there is no input
-    if (!guess) {
-      displayMessage('⛔ No Number!!');
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No Number!!');
 
-      // When player wins
-    } else if (guess === secretNumber) {
-      displayMessage('🎉 Correct Number!');
-      document.querySelector('.number').textContent = secretNumber;
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-      playing = false;
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    playing = false;
+  }
+  if (score > highScore) {
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+    // When guess is too high
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
     }
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
-      // When guess is too high
-    } else if (guess !== secretNumber) {
-      if (score > 1) {
-        document.querySelector('.message').textContent =
-          guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
-        score--;
-        document.querySelector('.score').textContent = score;
-      }
-    } else {
-      displayMessage('🎇 You lost the game!');
-      document.querySelector('.score').textContent = 0;
-    }
+  } else {
+    displayMessage('🎇 You lost the game!');
+    document.querySelector('.score').textContent = 0;
   }
 });
 
@@ -55,3 +53,4 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
